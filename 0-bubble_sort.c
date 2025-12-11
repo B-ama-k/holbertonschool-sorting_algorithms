f4fa10a3207d5aa14f330ea4da3ac4a1abc9bc0a
root <root@ip-10-42-14-40.ec2.internal>
2025-12-11 11:11:01 +0000

    add exo 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- *                using the Bubble sort algorithm
- * @array: The array to sort
- * @size: Size of the array
- *
- * Description: Prints the array after each swap.
+ *               using the Bubble sort algorithm
+ * @array: The array of integers to sort
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int temp;
 	int swapped;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
 				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
